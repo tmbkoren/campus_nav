b65bbbb0a47eddc200b12ba54e4010918d4479fc
tmbkoren <25588519+tmbkoren@users.noreply.github.com>
2024-11-04 15:16:10 -0800

    gathering data

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Campus Navigation Project
+
---
+++ data.py
@@ -0,0 +1,29 @@
+campusMap = {
+    "Nutwood Parking Structure": {},
+    "Eastside Parking Structure": {},
+    "State College Parking Structure": {},
+    "Parking Lot A": {},
+    "Parking Lot A(south)": {},
+    "Parking Lot C(east)": {},
+    "Parking Lot C(west)": {},
+    "Parking Lot E": {},
+    "Parking Lot G": {},
+    "Parking Lot F": {},
+    "Parking Lot I": {},
+    "Parking Lot R": {},
+    "Titan Shops": {},
+    "Titan Gym": {},
+    "Titan Sports Fields": {},
+    "Student Recreation Center": {},
+    "Titan Student Union": {},
+    "Langsdorf Hall": {},
+    "McCarthy Hall": {},
+    "Humanities Building": {},
+    "Pollak Library": {},
+    "Gordon Hall": {},
+    "KHS Building": {},
+
+}
+
+def getCampusMap():
+    return campusMap
